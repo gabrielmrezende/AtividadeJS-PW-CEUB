30b7bfb38ddfae8532c78ff3b1328ff0574f6781
Gabriel Rezende <gabrielrezendee03@gmail.com>
2025-05-04 14:22:43 -0300

    atividade 1

--- atividade-1/script1.js
+++ atividade-1/script1.js
@@ -2,4 +2,10 @@ const js_qht = ""; //Quantidade de horas trabalhadas no mês
 const js_vht = ""; //Valor da hora trabalhada
 const js_inss = ""; //Percentual de desconto do INSS
 
-
+if (js_qht < 135 || js_qht > 180) {
+  console.log("Quantidade de horas inválida (deve ser entre 135 e 180)");
+} else if (js_vht < 25 || js_vht > 70) {
+  console.log("Valor da hora inválido (deve ser entre 25 e 70)");
+} else if (js_inss < 5 || js_inss > 15) {
+  console.log("Percentual de INSS inválido (deve ser entre 5% e 15%)");
+}
